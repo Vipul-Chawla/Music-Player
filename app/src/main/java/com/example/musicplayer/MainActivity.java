703117ed6c893e5b6a50c47e6830a10c264007f6
vipul <vipulchawla2001@gmail.com>
2021-11-10 11:09:14 +0530

    Music Player

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    showadapter myadapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerview);
+        myadapter = new showadapter(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(myadapter);
+    }
+}
